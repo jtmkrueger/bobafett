6a1ee8a1866e0d7e12aa00d0bc0e27d118e4b27a
CirKu17 <CirKu17@gmail.com>
2012-10-13 12:14:41 +0300

    Update bobafett.zsh

--- bobafett.zsh
+++ bobafett.zsh
@@ -2,9 +2,9 @@ function bobafett() {
 
     echo "As you wish."
 
-    if ps -ef | grep -q $1 ; then pkill $1 &&  exit; fi
+    if ps -ef | grep -q $1 ; then pkill $1 &&  return; fi
 
-    if id -u $1 >/dev/null 2>&1; then userdel $1 && exit; fi
+    if id -u $1 >/dev/null 2>&1; then userdel $1 && return; fi
 
     find -name $1 -execdir shred -u {} +
     
