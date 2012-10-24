9ec5d5fcb2411b8253ad03c588df9492820af403
CirKu17 <CirKu17@gmail.com>
2012-10-24 22:42:14 +0300

    executing only first matching action ( issue #3 )

--- bobafett.zsh
+++ bobafett.zsh
@@ -3,12 +3,14 @@ function bobafett() {
     echo "As you wish."
 
     for i in  $@; do
-        if ps -ef | grep -q $i ; then pkill $i ; fi
-        
-        if id -u $i >/dev/null 2>&1; then userdel $i ; fi
-        
-        find -name $i -execdir shred -u {} +
+        if ps -ef | grep -q $i ; then 
+            pkill $i
+        elif id -u $i >/dev/null 2>&1; then 
+            userdel $i ; fi
+        else
+            find -name $i -execdir shred -u {} +
     done
 
 }
+
 
